d6998b8e736e06e6a28e358891e805466435de4c
filmgarage <30559371+filmgarage@users.noreply.github.com>
2025-10-26 16:37:38 +0100

    Update run.sh

--- shellyman/run.sh
+++ shellyman/run.sh
@@ -2,8 +2,5 @@
 #!/usr/bin/with-contenv bash
 set -euo pipefail
 
-ls -R /app
-ls -R /main
-
 export PORT="${INGRESS_PORT:-8099}"
-exec /venv/bin/python -m app.main
+exec /venv/bin/python -m main.py
